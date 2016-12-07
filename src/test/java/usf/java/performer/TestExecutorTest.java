354c368e3a2ed20f927d7b826e1fcc876257bed6
uSf <usf.alami@gmail.com>
2016-12-07 08:38:23 +0100

    evol

--- src/test/java/usf/java/performer/TestExecutorTest.java
+++ src/test/java/usf/java/performer/TestExecutorTest.java
@@ -12,34 +12,122 @@ public class TestExecutorTest {
 	
 	@Test
 	public void test1() {
-		genericTest(0);
+		testPositifNumber(0);
 	}
 	@Test
 	public void test2() {
-		genericTest(-1);
+		testPositifNumber(-1);
 	}
 	@Test
 	public void test3() {
-		genericTest(1, 2, 3, 4, 5);
+		testPositifNumber(1, 2, 3, 4, 5);
 	}
 	@Test
 	public void test4() {
-		genericTest(-1, 2, 3, 4, 5);
+		testPositifNumber(-1, 2, 3, 4, 5);
 	}
 	@Test
 	public void test5() {
-		genericTest(1, 2, 3, 4, -5);
+		testPositifNumber(1, 2, 3, 4, -5);
 	}
 	@Test
 	public void test6() {
-		genericTest(1, 2, -3, 4, 5);
+		testPositifNumber(1, 2, -3, 4, 5);
 	}
 	@Test
 	public void test7() {
-		genericTest(-1, -2, -3, -4, -5);
+		testPositifNumber(-1, -2, -3, -4, -5);
 	}
-
-	private void genericTest(Integer... values){
+	
+	@Test
+	public void test9() {
+		testPositifNumber2d(new Integer[][]{
+			{0}
+		});
+	}
+	@Test
+	public void test10() {
+		testPositifNumber2d(new Integer[][]{
+			{-1}
+		});
+	}
+	@Test
+	public void test11() {
+		testPositifNumber2d(new Integer[][]{
+			{1,2,3}
+		});
+	}
+	
+	@Test
+	public void test12() {
+		testPositifNumber2d(new Integer[][]{
+			{-1,2,3}
+		});
+	}
+	
+	@Test
+	public void test13() {
+		testPositifNumber2d(new Integer[][]{
+			{1,2,-3}
+		});
+	}	
+	
+	@Test
+	public void test14() {
+		testPositifNumber2d(new Integer[][]{
+			{0},{0},{0}
+		});
+	}
+	@Test
+	public void test15() {
+		testPositifNumber2d(new Integer[][]{
+			{-1},{-1},{0}
+		});
+	}
+	@Test
+	public void test16() {
+		testPositifNumber2d(new Integer[][]{
+			{1, 2, 3, 4},
+			{1, 2, 3, 4},
+			{1, 2, 3, 4}
+		});
+	}
+	@Test
+	public void test17() {
+		testPositifNumber2d(new Integer[][]{
+			{-1, 2, 3, 4},
+			{1, 2, 3, 4},
+			{1, 2, 3, 4}
+		});
+	}
+	@Test
+	public void test18() {
+		testPositifNumber2d(new Integer[][]{
+			{1, 2, 3, 4},
+			{1, 2, 3, 4},
+			{1, 2, 3, -4}
+		});
+	}
+	
+	@Test
+	public void test19() {
+		testPositifNumber2d(new Integer[][]{
+			{1, 2, 3, 4},
+			{1, 2, -3, 4},
+			{1, 2, 3, 4}
+		});
+	}
+	
+	@Test
+	public void test20() {
+		testPositifNumber2d(new Integer[][]{
+			{-1, -2, -3, -4},
+			{-1, -2, -3, -4},
+			{-1, -2, -3, -4}
+		});
+	}
+	
+	private void testPositifNumber(Integer... values){
 	
 		List<Integer> list = Arrays.asList(values);
 
@@ -52,18 +140,22 @@ public class TestExecutorTest {
 		TestCase.assertEquals(result, i==values.length);
 	}
 	
-	private void generic2dTest(Integer[]... values){
+	private void testPositifNumber2d(Integer[][] values){
 		
 		List<List<Integer>> list = new ArrayList<>();
-		for(int i=0; i<0; i++)
+		for(int i=0; i<values.length; i++)
 			list.add(Arrays.asList(values[i]));
 
 		usf.java.performer.Test<Integer> op = new PosetifNumberTest();
 		boolean result = TestExecutor.execute(op, list);
 		
-		int i = 0; 
-		while(i<values.length && values[i][j]>=0) i++;
-		
+		int i = 0, j = 0; 
+		while(i<values.length && values[i][j]>=0){
+			j++;
+			if(j == values[i].length){
+				j=0; i++;
+			}
+		}
 		TestCase.assertEquals(result, i==values.length);
 	}
 	
